515aeac7db3fb9174d819c7263091a094d6d7654
anuragsamota <anuragsamotasamota@gmail.com>
2023-05-01 15:59:51 +0530

    improved sound

--- js/tap_sound.js
+++ js/tap_sound.js
@@ -4,17 +4,3 @@ document.body.click = ()=>{
     tap_audio.currentTime = 0;
     tap_audio.play();
 };
-
-document.addEventListener('mousedown',()=>{
-    if (!tap_audio.playing){
-        tap_audio.currentTime = 0;
-        tap_audio.play();
-    }
-})
-
-document.addEventListener('touchstart',()=>{
-    if (!tap_audio.playing){
-        tap_audio.currentTime = 0;
-        tap_audio.play();
-    }
-})
