3d5e1c4d350781026a52b23f49886d9946963131
anuragsamota <anuragsamotasamota@gmail.com>
2023-05-01 13:01:03 +0530

    responsive payment carousel

--- js/carousel.js
+++ js/carousel.js
@@ -1,15 +1,25 @@
+// Main Carousel
+
 document.addEventListener( 'DOMContentLoaded', function () {
     new Splide( '#image-carousel',{
         type : 'loop',
+        lazyload : true,
         perPage : 1,
         autoplay : true,
         interval : 2000,
     } ).mount();
 } );
 
+
+
+
+
+//  Multi image carousel
+
 document.addEventListener( 'DOMContentLoaded', function () {
     new Splide( '#image-multi-carousel',{
     type   : 'loop',
+    lazyload : true,
     drag   : 'free',
     focus  : 'center',
     perPage: 3,
@@ -18,13 +28,40 @@ document.addEventListener( 'DOMContentLoaded', function () {
     } ).mount();
 } );
 
+
+
+
+// Payment Carousel
+function checkSize(){
+    let slides = 2.5
+    if (document.body.clientWidth < 640){
+        slides = 2.5
+    }else if(document.body.clientWidth <1024){
+        slides = 3.5
+    }else{
+        slides = 5
+    }
+    return slides;
+}
+
+
+
 document.addEventListener( 'DOMContentLoaded', function () {
     new Splide( '#payment-carousel',{
-    type   : 'loop',
-    drag   : 'free',
-    focus  : 'center',
-    perPage: 4,
-    autoplay : true,
-    interval : 1000,
-    } ).mount();
-} );
+        type   : 'loop',
+        lazyload : true,
+        drag   : 'free',
+        focus  : 'center',
+        perPage: 5,
+        breakpoints: {
+              640: {
+                  perPage: 1.5,
+              },
+              1024 : {
+                perPage: 3.5,
+              }
+        },
+        autoplay : true,
+        interval : 1000,
+    }).mount();
+});
